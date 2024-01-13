fde1d152fb85dab698ea7affe17084ac50f15282
THZ <thz15916108729@163.com>
2024-01-13 20:41:14 +0800

    bv0.0.0-d-a-1

--- Views/Pages/DashboardPage.xaml.cs
+++ Views/Pages/DashboardPage.xaml.cs
@@ -1,20 +1,14 @@
 ﻿using Aurora_Star_Launcher.ViewModels.Pages;
 using Wpf.Ui.Controls;
-using MinecraftLaunch.Classes.Models.Auth;
-using MinecraftLaunch.Classes.Models.Launch;
-using KMCCC.Launcher;
 using StarLight_Core.Authentication;
 using System.Diagnostics;
 using MessageBox = System.Windows.MessageBox;
+using StarLight_Core.Models.Launch;
 
 namespace Aurora_Star_Launcher.Views.Pages
 {
     public partial class DashboardPage : INavigableView<DashboardViewModel>
     {
-        public static LaunchConfig LaunchConfig { get; } = new LaunchConfig();
-        public Account UserInfo { get; private set; }
-
-        public static LauncherCore Core = LauncherCore.Create();
         private string versionURL;
 
         public DashboardViewModel ViewModel { get; }
@@ -27,12 +21,13 @@ namespace Aurora_Star_Launcher.Views.Pages
             InitializeComponent();
 
             // 自动寻找版本
-            var versions = Core.GetVersions().ToArray();
+            var versions = StarLight_Core().ToArray();
             version.ItemsSource = versions;//绑定数据源
             version.DisplayMemberPath = "Id";//设置comboBox显示的为版本Id
 
             // 初始选择
-            version.SelectedItem = 1;
+            version.SelectedIndex = 1;
+            SettingsPage.JavaList.SelectedIndex = 1;
 
         }
 
@@ -65,41 +60,39 @@ namespace Aurora_Star_Launcher.Views.Pages
         // a Microsoft E
 
         // b 离线 S
-        private void Offine_Start_Click (object sender, RoutedEventArgs e)
+        private async void Offine_Start_Click (object sender, RoutedEventArgs e)
         {
             if (Offine_User_Name.Text != string.Empty && SettingsPage.JavaList.Text != string.Empty && version.Text != string.Empty && SettingsPage.MemoryBox.Text != string.Empty)
             {
                 try
                 {
-                    Core.JavaPath = SettingsPage.JavaList.SelectedValue + "\\javaw.exe";
-                    var ver = (KMCCC.Launcher.Version)version.SelectedItem;
-                    var result = Core.Launch(new LaunchOptions
+                    string verText = version.SelectedItem.ToString();
+                    var auth = new OfflineAuthentication(Offine_User_Name.Text);
+                    var account = await auth.Name.();
+                    LaunchConfig args = new()
                     {
-                        Version = ver,
-                        MaxMemory = Convert.ToInt32(SettingsPage.MemoryBox.Text),
-                        Authenticator = new KMCCC.Authentication.OfflineAuthenticator(Offine_User_Name.Text),
-                        Mode = LaunchMode.MCLauncher,
-
-                    });
-                    // 错误提示
-                    if (!result.Success)
-                    {
-                        switch (result.ErrorType)
+                        GameWindowConfig = new()
                         {
-                            case ErrorType.NoJAVA:
-                                MessageBox.Show("Java错误！详细信息：" + result.ErrorMessage, "错误！");
-                                break;
-                            case ErrorType.AuthenticationFailed:
-                                MessageBox.Show("登录时发生错误！详细信息：" + result.ErrorMessage, "错误！");
-                                break;
-                            case ErrorType.UncompressingFailed:
-                                MessageBox.Show("文件错误！详细信息：" + result.ErrorMessage, "错误！");
-                                break;
-                            default:
-                                MessageBox.Show(result.ErrorMessage, "错误！");
-                                break;
+                            Height = int.Parse(SettingsPage.GameH.Text),
+                            Width = int.Parse(SettingsPage.GameW.Text),
+                            IsFullScreen = false,
+                        },
+                        Account = new()
+                        {
+                            BaseAccount = account
+                        },
+                        GameCoreConfig = new()
+                        {
+                            Root = ".minecraft",
+                            Version = verText,
+                        },
+                        JavaConfig = new()
+                        {
+                            JavaPath = SettingsPage.JavaList.SelectedValue + "\\javaw.exe"
                         }
-                    }
+                    };
+                    var launch = new MinecraftLaunch(args);
+                    var la = await launch.Lanchasymc(ReportProgess);
                 }
                 catch
                 {
@@ -122,20 +115,47 @@ namespace Aurora_Star_Launcher.Views.Pages
         // c-b 统一通行证 S
         private async void External_Start_02_Click(object sender, RoutedEventArgs e)
         {
-            var auth = new UnifiedPassAuthenticator(External_02_User_Name.Text, External_02_User_Password.Text, External_02_ServerID.Text);
-            var account = auth.UnifiedPassAuthAsync();
-            LaunchConfig args = new()
+            if (External_02_User_Name.Text != string.Empty && External_02_User_Password.Text != string.Empty && External_02_ServerID.Text != string.Empty && SettingsPage.JavaList.Text != string.Empty && version.Text != string.Empty && SettingsPage.MemoryBox.Text != string.Empty)
             {
-                GameWindowConfig = new()
+                try
                 {
-                    Height = int.Parse(SettingsPage.GameH.Text),
-                    Width = int.Parse(SettingsPage.GameW.Text),
-                },
-                Account = new()
+                    string verText = version.SelectedItem.ToString();
+                    var auth = new UnifiedPassAuthenticator(External_02_User_Name.Text, External_02_User_Password.Text, External_02_ServerID.Text);
+                    var account = await auth.UnifiedPassAuthAsync();
+                    LaunchConfig args = new()
+                    {
+                        GameWindowConfig = new()
+                        {
+                            Height = int.Parse(SettingsPage.GameH.Text),
+                            Width = int.Parse(SettingsPage.GameW.Text),
+                            IsFullScreen = false,
+                        },
+                        Account = new()
+                        {
+                            BaseAccount = account
+                        },
+                        GameCoreConfig = new()
+                        {
+                            Root = ".minecraft",
+                            Version = verText,
+                        },
+                        JavaConfig = new()
+                        {
+                            JavaPath = SettingsPage.JavaList.SelectedValue + "\\javaw.exe"
+                        }
+                    };
+                    var launch = new MinecraftLaunch(args);
+                    var la = await launch.Lanchasymc(ReportProgess);
+                }
+                catch
                 {
-
-                },
-            };
+                    MessageBox.Show("启动失败！", "错误！");
+                }
+            }
+            else
+            {
+                MessageBox.Show("信息不完整！", "错误！");
+            }
         }
         // c-a 统一通行证 E
         // c 第三方 E
